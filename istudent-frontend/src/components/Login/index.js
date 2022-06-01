81ef946da40b9dff4e769d036a66ca97c5858fd9
gabrielonishi <gabrielonishi@outlook.com>
2022-05-31 22:00:08 -0300

    Página de Login devolve token

--- istudent-frontend/src/components/Login/index.js
+++ istudent-frontend/src/components/Login/index.js
@@ -4,16 +4,26 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { BrowserRouter as Router,Route,Link} from 'react-router-dom';
 import { useEffect, useState } from "react";
+import axios from "axios";
+import { Typography } from "@mui/material";
 
 
 function Login() {
 
     const [emailLogin, setEmailLogin] = useState('');
     const [passwordLogin, setPasswordLogin] = useState('');
+    const [loginError, setLoginError] = useState(false)
+    const [redirect, setRedirect] = useState(false)
 
-    console.log(emailLogin);
-    console.log(passwordLogin);
-
+    const submitData = () => {
+        axios
+        .post("http://127.0.0.1:8000/api/token/", {
+            "username": emailLogin,
+            "password":passwordLogin
+            })
+        .then((response) => console.log(response.data.token))
+        .catch((error) => setLoginError(true))
+    }
 
     return (
         <div className="login-content">
@@ -24,15 +34,37 @@ function Login() {
                 <div className="text-email">
                     <TextField id="email-basic" label="E-mail" onChange={(event) => setEmailLogin(event.target.value)}></TextField>
                 </div>
-                <div className="text-senha">
-                    <TextField id="senha-basic" label="senha" onChange={(event) => setPasswordLogin(event.target.value)}></TextField>
-                </div>
-                <div className="text-login">
-                    <Link to="/signup" style={{textDecoration:'none'}}>
-                    <Button sx={{color: 'black', fontWeight: 'bold', backgroundColor:'white', borderRadius: 4.5,textTransform: 'none', border: 3, borderColor:'#ce0303'}}>Cadastre-se</Button>
-                    </Link>
-                    <Button sx={{color: 'white', fontWeight: 'bold', backgroundColor:'#ce0303', borderRadius: 4.5,textTransform: 'none', border: 3, borderColor:'#ce0303'}}>LogIn</Button>
-                </div>
+                {/* <div className="text-senha">
+                    <TextField id="senha-basic" type="password" label="senha" onChange={(event) => setPasswordLogin(event.target.value)}></TextField>
+                </div> */}
+                {loginError==true?(
+                    <div>
+                        <div className="text-senha2">
+                        <TextField id="senha-basic" type="password" label="senha" onChange={(event) => setPasswordLogin(event.target.value)}></TextField>
+                        </div>
+                        <div className="bottom-login">
+                            Erro de Login, tente novamente
+                            <div className="text-login2">
+                                <Link to="/signup" style={{textDecoration:'none'}}>
+                                <Button sx={{color: 'black', fontWeight: 'bold', backgroundColor:'white', borderRadius: 4.5,textTransform: 'none', border: 3, borderColor:'#ce0303'}}>Cadastre-se</Button>
+                                </Link>
+                                <Button onClick={submitData}sx={{color: 'white', fontWeight: 'bold', backgroundColor:'#ce0303', borderRadius: 4.5,textTransform: 'none', border: 3, borderColor:'#ce0303'}}>LogIn</Button>
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        <div className="text-senha">
+                            <TextField id="senha-basic" type="password" label="senha" onChange={(event) => setPasswordLogin(event.target.value)}></TextField>
+                        </div>
+                        <div className="text-login">
+                            <Link to="/signup" style={{textDecoration:'none'}}>
+                            <Button sx={{color: 'black', fontWeight: 'bold', backgroundColor:'white', borderRadius: 4.5,textTransform: 'none', border: 3, borderColor:'#ce0303'}}>Cadastre-se</Button>
+                            </Link>
+                            <Button onClick={submitData}sx={{color: 'white', fontWeight: 'bold', backgroundColor:'#ce0303', borderRadius: 4.5,textTransform: 'none', border: 3, borderColor:'#ce0303'}}>LogIn</Button>
+                        </div>
+                    </div>
+                )}
             </Paper>
         </div>
     );
